b3a898bcd7a0eff28e6aadf938c7f53953ceb04d
mikkelpm <mikkel.p.m@gmail.com>
2021-07-05 19:36:37 -0400

    + block diagonal var-cov + efficient weighting for general case

--- worstcase_se.py
+++ worstcase_se.py
@@ -4,6 +4,7 @@ import scipy.optimize as opt
 import cvxopt as cvx
 from statsmodels.regression.quantile_regression import QuantReg
 from scipy.stats import chi2, norm
+from scipy.linalg import block_diag
 
 
 """Worst-case standard errors for minimum distance estimates
@@ -19,32 +20,62 @@ Cocci, Matthew D. & Mikkel Plagborg-Moller, "Standard Errors for Calibrated Para
 https://scholar.princeton.edu/mikkelpm/calibration
 """
 
+
 class MinDist:
     
     
     def __init__(self, moment_fct, moment_estim,
-                 moment_se=None, moment_varcov=None, moment_fct_deriv=None,
-                 zero_thresh=1e-8):
+                 moment_se=None, moment_varcov=None, moment_fct_deriv=None):
         
         self.moment_fct = moment_fct
         self.moment_estim = np.asarray(moment_estim).flatten()
-        self.moment_se = np.asarray(moment_se).flatten()
-        self.moment_varcov = np.asarray(moment_varcov)
-        self.zero_thresh = zero_thresh
         self.moment_num = len(moment_estim)
-        self.full_info = (moment_varcov is not None)
         self.moment_fct_deriv = self._deriv(moment_fct_deriv, self.moment_fct)
         
+        # Var-cov matrix
+        assert (moment_se is None) + (moment_varcov is None) == 1, 'Either "moment_se" or "moment_varcov" must be supplied, but not both'
+        if moment_varcov is not None:
+            self.moment_varcov = np.asarray(moment_varcov)
+        else:
+            self.moment_varcov = np.empty((self.moment_num,self.moment_num))
+            self.moment_varcov[np.eye(self.moment_num)==1] = np.asarray(moment_se)
+            self.moment_varcov[np.eye(self.moment_num)==0] = np.nan
+        
         # Check inputs
-        assert (moment_se is not None) or (moment_varcov is not None), 'Either "moment_se" or "moment_varcov" must be supplied'
-        assert (moment_se is None) or (len(self.moment_se)==self.moment_num), 'Dimension of "moment_se" does not match "moment_estim"'
-        assert (moment_varcov is None) or (self.moment_varcov.shape==(self.moment_num,self.moment_num)), 'Dimension of "moment_varcov" is wrong'
-        assert np.all(np.isreal(self.moment_estim)), 'Wrong input type for "moment_estim"'
-        assert np.all(np.isreal(self.moment_se)), 'Wrong input type for "moment_se"'
-        assert np.all(np.isreal(self.moment_varcov)), 'Wrong input type for "moment_varcov"'
-        assert np.isscalar(self.zero_thresh) and (self.zero_thresh>=0), '"zero_thresh" must be a positive scalar'
+        assert self.moment_varcov.shape==(self.moment_num,self.moment_num), 'Dimension of "moment_se" or "moment_varcov" is wrong'
+        assert np.isreal(self.moment_estim).all(), 'Wrong input type for "moment_estim"'
+        assert np.isreal(self.moment_varcov).all(), 'Wrong input type for "moment_se" or "moment_varcov"'
+        assert (np.diag(self.moment_varcov)>=0).all(), 'SE for each individual moment must be nonnegative'
+        assert np.any([self.moment_varcov==self.moment_varcov.T, np.isnan(self.moment_varcov)],axis=0).all(), '"moment_varcov" must be symmetric'
         assert self.moment_num>=1, '"moment_estim" must have at least one element'
         
+        # Determine type of var-cov matrix
+        self.full_info = (not np.isnan(self.moment_varcov).any()) # Full info
+        self.diag_only = ((not self.full_info) and np.isnan(self.moment_varcov[np.eye(self.moment_num)==0]).all()) # Only diagonal is known
+        
+        # Check if var-cov has known block diagonal (and unknown everywhere else)
+        self.blockdiag_only = False
+        if not (self.full_info or self.diag_only):
+            i = 0
+            block_inds = []
+            while i<self.moment_num: # Loop through purported blocks
+                the_block = i + np.flatnonzero(1-np.isnan(self.moment_varcov[i,i:]))
+                if not (np.diff(the_block)==1).all():
+                    return # Can't be block diagonal
+                block_inds.append(the_block)
+                i = the_block.max()+1
+            # Check that the block diagonal indeed has non-NaN values, with NaN's outside blocks
+            block_bool = block_diag(*[np.ones((len(b),len(b))) for b in block_inds])
+            self.blockdiag_only = (not np.isnan(self.moment_varcov[block_bool==1]).any()) and np.isnan(self.moment_varcov[block_bool==0]).all()
+            if not self.blockdiag_only:
+                return
+        
+            # If block diagonal, extract useful information for later
+            self.moment_varcov_blocks = {'ind': block_inds, # Indices of blocks
+                                         'varcov': [self.moment_varcov[np.ix_(b,b)] for b in block_inds], # Var-cov for each block
+                                         'num': len(block_inds)}
+            self.moment_varcov_blocks['chol'] = [(b if b.ndim==1 else np.linalg.cholesky(b)) for b in self.moment_varcov_blocks['varcov']] # Cholesky factors
+            
     
     def fit(self, transf=lambda x: x, weight_mat=None,
             opt_init=None, estim_fct=None, eff=True, one_step=True, transf_deriv=None,
@@ -65,16 +96,15 @@ class MinDist:
         if self.full_info and (eff or (weight_mat is None)):
             weight_mat = np.linalg.inv(self.moment_varcov) # Full-info efficient weight matrix
         if weight_mat is None:
-            weight_mat = np.diag(self.moment_se**(-2)) # Ad hoc diagonal weight matrix
+            weight_mat = np.diag(1/np.diag(self.moment_varcov)) # Ad hoc diagonal weight matrix
         
         # Default estimation routine
         if estim_fct is None:
-            estim_fct = lambda W: opt.fmin_bfgs(lambda x: (self.moment_estim-self.moment_fct(x)) @ W @ np.reshape(self.moment_estim-self.moment_fct(x),(-1,1)), opt_init)
+            estim_fct = lambda W: opt.minimize(lambda x: (self.moment_estim-self.moment_fct(x)) @ W @ (self.moment_estim-self.moment_fct(x)), opt_init, method='BFGS')['x']
         
         # Initial estimate of parameters, if not supplied
         if param_estim is None:
             param_estim = estim_fct(weight_mat)
-        param_num = len(param_estim)
         
         # Transformation, moment function, and Jacobians at initial estimate
         transf_estim, transf_jacob, moment_fit, moment_jacob \
@@ -101,11 +131,7 @@ class MinDist:
                 if transf_num > 1: # If more than one parameter of interest, handle each separately by recursive call
                 
                     transf_estim_init = transf_estim.copy()
-                    transf_estim = np.empty(transf_num)
-                    transf_estim_se = np.empty(transf_num)
-                    moment_loadings = np.empty((self.moment_num,transf_num))
-                    for i in range(transf_num):
-                        the_res = self.fit(transf=lambda x: transf(x)[i],
+                    ress = [self.fit(transf=lambda x: transf(x)[i],
                                            weight_mat=weight_mat,
                                            estim_fct=estim_fct,
                                            eff=True, one_step=one_step,
@@ -113,32 +139,29 @@ class MinDist:
                                            transf_estim=transf_estim_init[i],
                                            transf_jacob=transf_jacob[i,:],
                                            moment_fit=moment_fit,
-                                           moment_jacob=moment_jacob) # Computations for scalar parameter of interest
-                        transf_estim[i] = the_res['transf_estim']
-                        transf_estim_se[i] = the_res['transf_estim_se']
-                        moment_loadings[:,i] = the_res['moment_loadings']
-                        
+                                           moment_jacob=moment_jacob)
+                            for i in range(transf_num)] # Compute for each parameter of interest
+                    transf_estim = np.array([r['transf_estim'] for r in ress])
+                    transf_estim_se = np.array([r['transf_estim_se'] for r in ress])
+                    moment_loadings = np.array([r['moment_loadings'] for r in ress]).T
+                    weight_mat = [r['weight_mat'] for r in ress]
+                    
                 else: # If only single parameter of interest
                 
-                    transf_estim_se, moment_loadings = self.worstcase_eff(moment_jacob, transf_jacob)
+                    transf_estim_se, moment_loadings, weight_mat = self.worstcase_eff(moment_jacob, transf_jacob, weight_mat=weight_mat)
                     if one_step: # One-step estimation
-                        transf_estim = self._get_onestep(moment_fit, None, moment_loadings, transf_estim)
+                        transf_estim = self._get_onestep(moment_fit, None, moment_loadings, transf_estim).item()
                     else: # Full optimization estimation
-                        # Weight matrix puts weight on only k moments, where k=#parameters
-                        sort_inds = np.abs(moment_loadings).argsort()
-                        weight_mat_new = weight_mat.copy()
-                        weight_mat_new[sort_inds[:self.moment_num-param_num],:] = 0
-                        weight_mat_new[:,sort_inds[:self.moment_num-param_num]] = 0
-                        param_estim = estim_fct(weight_mat_new)
+                        param_estim = estim_fct(weight_mat)
                         transf_estim = transf(param_estim)
         
         # Start building results dictionary
         res = {'transf_estim': transf_estim,
                'param_estim': param_estim,
-               'weight_mat': weight_mat if (self.full_info or (not eff)) else None,
+               'weight_mat': weight_mat,
                'moment_fit': moment_fit,
                'moment_jacob': moment_jacob,
-               'moment_loadings': moment_loadings if (self.full_info or (not eff)) else None,
+               'moment_loadings': moment_loadings,
                'transf_jacob': transf_jacob,
                'transf_num': transf_num}
         
@@ -152,7 +175,8 @@ class MinDist:
                 # Do nothing, since standard errors have already been computed above
                 pass
             else:
-                transf_estim_se = self.moment_se @ np.abs(moment_loadings)
+                transf_estim_se, worstcase_varcov = self.worstcase_se(moment_loadings)
+                res['worstcase_varcov'] = worstcase_varcov
         
         res['transf_estim_se'] = transf_estim_se
         
@@ -179,13 +203,13 @@ class MinDist:
                 test_weight_mat = np.linalg.inv(estim_res['transf_estim_varcov'])
             else: # Limited information
                 # Ad hoc choice motivated by independence
-                test_weight_mat = np.linalg.inv(estim_res['moment_loadings'].T @ np.diag(self.moment_se**2) @ estim_res['moment_loadings'])
+                test_weight_mat = np.linalg.inv(estim_res['moment_loadings'].T @ np.diag(np.diag(self.moment_varcov)) @ estim_res['moment_loadings'])
         
         # Check dimensions
         assert test_weight_mat.shape == (estim_res['transf_num'],estim_res['transf_num']), 'Dimension of "test_weight_mat" is wrong'
         
         # Test statistic
-        joint_stat = estim_res['transf_estim'] @ test_weight_mat @ estim_res['transf_estim'].reshape(-1,1)
+        joint_stat = estim_res['transf_estim'] @ test_weight_mat @ estim_res['transf_estim']
         
         # p-value
         if self.full_info:
@@ -209,6 +233,8 @@ class MinDist:
         
         """Over-identification test
         """
+        
+        assert isinstance(estim_res['weight_mat'], np.ndarray), 'Estimation results must be based on a single weight matrix'
         
         # Errors in fitting moments
         moment_error = self.moment_estim - estim_res['moment_fit']
@@ -247,44 +273,125 @@ class MinDist:
         return res
         
     
-    def worstcase_eff(self, moment_jacob, transf_jacob):
+    def worstcase_se(self, moment_loadings):
         
-        """Compute worst-case efficient moment loadings via median regression
+        """Worst-case standard errors and corresponding var-cov matrix
+        for linear combination of moments
+        """
+        
+        if moment_loadings.ndim>1: # If more than one parameter of interest, handle them separately
+            ress = [self.worstcase_se(moment_loadings[:,i]) for i in range(moment_loadings.shape[1])]
+            return np.array([r[0] for r in ress]), [r[1] for r in ress]
+        
+        if self.diag_only: # Only diagonal is known
+        
+            moment_se = np.sqrt(np.diag(self.moment_varcov))
+            se = moment_se @ np.abs(moment_loadings) # Closed form
+            aux = np.sign(moment_loadings) * moment_se
+            varcov = np.outer(aux, aux)
+            
+        elif self.blockdiag_only: # Only block diagonal is known
+        
+            loading_blocks = [moment_loadings[ind] for ind in self.moment_varcov_blocks['ind']]
+            aux = [self.moment_varcov_blocks['varcov'][i] \
+                   @ loading_blocks[i]
+                   for i in range(self.moment_varcov_blocks['num'])]
+            var_blocks = [loading_blocks[i] @ aux[i]
+                          for i in range(self.moment_varcov_blocks['num'])]
+            se = np.sqrt(var_blocks).sum()
+            aux2 = [aux[i]/np.sqrt(var_blocks[i]) for i in range(self.moment_varcov_blocks['num'])]
+            aux3 = [self.moment_varcov_blocks['chol'][i] \
+                    - np.outer(aux[i], loading_blocks[i] @ self.moment_varcov_blocks['chol'][i]) \
+                    / var_blocks[i]
+                    for i in range(self.moment_varcov_blocks['num'])]
+            aux4 = np.hstack((np.hstack(aux2).reshape(-1,1), block_diag(*aux3)))
+            varcov = aux4 @ aux4.T
+            
+        else: # General knowledge of var-cov matrix
+        
+            # Solve semidefinite programming problem
+            var, varcov = self.solve_sdp(np.outer(moment_loadings, moment_loadings))
+            se = np.sqrt(var)
+            varcov = np.array(varcov) # Convert to numpy array
+            
+        return se, varcov
+    
+    
+    def worstcase_eff(self, moment_jacob, transf_jacob, weight_mat=None):
+        
+        """Compute worst-case efficient moment loadings and weight matrix
         See main paper for explanation
         """
         
         # Set up median regression as described in paper
         GpG = moment_jacob.T @ moment_jacob
-        Y = self.moment_se.reshape(-1,1) * (moment_jacob @ np.linalg.solve(GpG, transf_jacob.reshape(-1,1)))
+        Y = moment_jacob @ np.linalg.solve(GpG, transf_jacob.reshape(-1,1))
         val, vec = np.linalg.eig(np.eye(self.moment_num) - moment_jacob @ np.linalg.solve(GpG, moment_jacob.T))
-        moment_jacob_perp = vec[:, abs(val)>self.zero_thresh]
-        X = -self.moment_se.reshape(-1,1) * moment_jacob_perp
-    
-        # Run median regression
-        qr_mod = QuantReg(Y, X)
-        qr_fit = qr_mod.fit(q=.5)
-        resid = qr_fit._results.resid # Residuals
+        (p,k) = moment_jacob.shape
+        moment_jacob_perp = vec[:, np.abs(val).argsort()[:p-k]]
+        X = -moment_jacob_perp
         
-        # Efficient moment loadings and standard errors
-        moment_loadings = resid / self.moment_se
-        se = np.abs(resid).sum()
+        if self.diag_only: # Only diagonal is known
         
-        return se, moment_loadings
+            # Run median regression
+            moment_se = np.sqrt(np.diag(self.moment_varcov))
+            qr_mod = QuantReg(moment_se.reshape(-1,1) * Y, moment_se.reshape(-1,1) * X)
+            qr_fit = qr_mod.fit(q=.5)
+            resid = qr_fit._results.resid # Residuals
+            moment_loadings = resid / moment_se
+            se = np.abs(resid).sum()
+            
+            # Weight matrix puts weight on only k moments
+            sort_inds = np.abs(moment_loadings).argsort()
+            weight_mat_new = weight_mat.copy()
+            weight_mat_new[sort_inds[:p-k],:] = 0
+            weight_mat_new[:,sort_inds[:p-k]] = 0
+            
+        else: # General case
+            
+            # Objective function and gradient
+            def objective(z, Y, X):
+                resid = Y.flatten() - X @ z
+                se, varcov = self.worstcase_se(resid)
+                grad = -2 * (X.T @ varcov @ resid)
+                return se**2, grad
+        
+            # Solve nested optimization
+            cvx.solvers.options['show_progress'] = False # Suppress CVX output temporarily
+            opt_res = opt.minimize(lambda z: objective(z, Y, X), np.zeros(p-k), jac=True, method='BFGS')
+            cvx.solvers.options['show_progress'] = True
+            moment_loadings = Y.flatten() - X @ opt_res['x']
+            se = np.sqrt(opt_res['fun'])
+            
+            # Weight matrix
+            aux1 = np.outer(transf_jacob, opt_res['x']) / (transf_jacob @ np.linalg.solve(GpG, transf_jacob.reshape(-1,1)))
+            W = lambda delta: np.vstack((np.hstack((np.eye(k),aux1)),np.hstack((aux1.T,delta*np.eye(p-k)))))
+            # Determine delta such that W(delta) is positive definite
+            delta_pd = opt.fsolve(lambda delta: np.linalg.eigvalsh(W(delta)).min()-0.01, 0)
+            aux2 = np.hstack((moment_jacob, moment_jacob_perp))
+            weight_mat_new = aux2 @ W(delta_pd) @ aux2.T
+            
+        return se, moment_loadings, weight_mat_new
     
     
     def solve_sdp(self, A):
         
         """Solve semidefinite programming problem
-        max tr(A*V) s.t. V psd and diag(V)=self.moment_se**2
+        max tr(A*V) s.t. V psd and known elements of V
         """
         
+        # Find elements of V with known values (below diagonal)
+        inds = np.all([(np.tril(np.ones((self.moment_num,self.moment_num)))==1),1-np.isnan(self.moment_varcov)],axis=0)
+        
         # Coefficient matrices
-        c = cvx.matrix(-self.moment_se.reshape(-1,1)**2,tc='d')
-        G = cvx.sparse(cvx.matrix(np.eye(self.moment_num**2)[:,np.eye(self.moment_num).flatten()==1]))
+        c = cvx.matrix(-self.moment_varcov[inds.T].reshape(-1,1),tc='d')
+        G = cvx.sparse(cvx.matrix(np.eye(self.moment_num**2)[:,inds.T.flatten()==1]))
         h = cvx.matrix(-A,tc='d')
         
         # Solve SDP
-        sol = cvx.solvers.sdp(c,Gs=[G],hs=[h])
+        V_init = self.moment_varcov.copy()
+        V_init[np.isnan(V_init)]=0
+        sol = cvx.solvers.sdp(c,Gs=[G],hs=[h],dualstart={'zs': [cvx.matrix(V_init)]})
         
         # Return objective value and optimal V
         return sol['dual objective'], sol['zs'][0]
@@ -315,9 +422,9 @@ class MinDist:
         """
         
         if weight_mat is None:
-            subtr = moment_jacob.T @ np.reshape(moment_init-self.moment_estim,(-1,1))
+            subtr = moment_jacob.T @ (moment_init-self.moment_estim)
         else:
-            subtr = np.linalg.solve(moment_jacob.T @ weight_mat @ moment_jacob, moment_jacob.T @ weight_mat @ np.reshape(moment_init-self.moment_estim,(-1,1)))
+            subtr = np.linalg.solve(moment_jacob.T @ weight_mat @ moment_jacob, moment_jacob.T @ weight_mat @ (moment_init-self.moment_estim))
         return param_init - subtr.flatten()
     
     
