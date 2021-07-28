00a8c86c4b16f3b84e568148373f56d2f98398cf
mikkelpm <mikkel.p.m@gmail.com>
2021-07-28 15:42:10 -0400

    Actual data for HANK application

--- estimate_hank.py
+++ estimate_hank.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.optimize as opt
 from scipy.stats import norm
 from scipy.signal import lfilter
+from pandas import read_csv
 from sequence_jacobian import hank
 import sequence_jacobian.jacobian as jac
 from stderr_calibration import MinDist
@@ -19,35 +20,36 @@ using functions produced by Auclert, Bardozcy, Rognlie & Straub (ECMA, 2021)
 print('Computing steady state...')
 ss = hank.hank_ss(beta_guess=.982, vphi_guess=0.786)
 print('Done.')
-T = 300 # Time horizon for equilibrium calculations
 
 
 """Part 1b: Empirical moments (impulse responses)
 """
 
-horzs = [0,4,8] # Response horizons
-# TFP shock (Chang, Chen & Schorfheide, 2021)
-irf_z_data =  {'Z': np.array([2,2.1,2.05]), # Log TFP
-               'Z_se': np.array([0.2,0.7,0.7])/norm.ppf(0.9)/2,
-               'Y': np.array([0.8,1.8,1.7]), # Log output
-               'Y_se': np.array([0.2,0.8,0.8])/norm.ppf(0.9)/2,
-               'PctBelowGDP': (np.array([43.8,43.7,43.5])-43.4)/3, # % earning below 2/3 of per capita GDP
-               'PctBelowGDP_se': np.array([1.1,0.7,0.4])/norm.ppf(0.9)/2/3}
-# Note: norm.ppf(0.9) is due to the paper reporting 80% credible bands
-# For "PctBelowGDP" we further divide by 3 as the paper reports responses to 3 s.d. shock
+horzs = [0,4,8] # Response horizons (quarters after shock)
 
-# MP shock (Miranda-Agrippino & Ricco, 2021)
-irf_mp_data = {'Y': np.array([-1,-1.6,-1.4]), # Log output
-               'Y_se': np.array([0.5,1.3,1.8])/norm.ppf(0.95)/2,
-               'P': np.array([-0.25,-0.5,-0.65]), # Log price level
-               'P_se': np.array([0.17,0.5,0.7])/norm.ppf(0.95)/2,
-               '1Y': np.array([1,0.15,0]), # 1-year bond rate
-               '1Y_se': np.array([0,0.45,0.4])/norm.ppf(0.95)/2}
+# TFP shock (Chang, Chen & Schorfheide, 2021, Figures 7 and 9)
+data_ccs = read_csv('data/chang_chen_schorfheide.csv')
+irf_z_data = {}
+for v in ['TFP','GDP','BelowCutoff']:
+    irf_z_data[v] = data_ccs[v+'_IRF_q50'].values[1:][horzs]/3
+    irf_z_data[v+'_se'] = (data_ccs[v+'_IRF_q90'].values[1:][horzs]-data_ccs[v+'_IRF_q10'].values[1:][horzs])/3/(2*norm.ppf(0.9))
+# Note: norm.ppf(0.9) is due to the paper reporting 80% credible bands
+# We divide by 3 so we get 1 s.d. TFP shock
+
+# MP shock (Miranda-Agrippino & Ricco, 2021, Figure 3)
+data_mar = read_csv('data/mirandaagrippino_ricco.csv')
+horzs_mth = np.array(horzs)*3 # Data is monthly, so we use last response in each quarter of interest
+irf_mp_data = {}
+for v in ['INDPRO','CPIAUCSL','GS1']:
+    irf_mp_data[v] = data_mar[v+'_IRF_q50'].values[horzs_mth]
+    irf_mp_data[v+'_se'] = (data_mar[v+'_IRF_q95'].values[horzs_mth]-data_mar[v+'_IRF_q05'].values[horzs_mth])/(2*norm.ppf(0.95))
 # Note: norm.ppf(0.95) is due to the paper reporting 90% credible bands
 
 
-""" Part 2: model-based IRF functions
+""" Part 2: Model-based IRF functions
 """
+
+T = 300 # Time horizon for equilibrium calculations
 
 # Compute Jacobian of household block once and for all (only depends on non-estimated parameters)
 print('Computing Jacobian of household block...')
@@ -82,10 +84,10 @@ def get_irf(phi, kappa, ar_z, ma_z, sigma_z, ar_mp, ma_mp, horzs, ss, T):
     # TFP shock responses
     dz = ss['Z'] * lfilter([1,ma_z],[1,-ar_z],np.insert(np.zeros(T-1),0,sigma_z)) # ARMA(1,1) shock to TFP
     dY_dz = dz @ G['Y']['Z'][horzs,:].T / ss['Y'] # IRF of log output
-    dPctBelowGDP_dz = dz @ G['EARN_LT_GDP']['Z'][horzs,:].T # IRF of percentage of people earning less than GDP
+    dPctBelowGDP_dz = dz @ G['EARN_LT_GDP']['Z'][horzs,:].T # IRF of fraction of people earning less than GDP
     irf_z = {'Z': 100 * dz[horzs] / ss['Z'], # IRF of log TFP
              'Y': 100 * dY_dz,
-             'PctBelowGDP': 100 * dPctBelowGDP_dz}
+             'PctBelowGDP': dPctBelowGDP_dz}
     
     # MP shock responses
     dmp = lfilter([1,ma_mp],[1,-ar_mp],np.insert(np.zeros(T-1),0,1)) # ARMA(1,1) shock to Taylor rule
@@ -97,7 +99,7 @@ def get_irf(phi, kappa, ar_z, ma_z, sigma_z, ar_mp, ma_mp, horzs, ss, T):
     # Normalize MP shock responses
     irf_mp = {'Y': dY_dmp/d1y_dmp[0],
               'P': dP_dmp/d1y_dmp[0],
-              '1Y': d1y_dmp/d1y_dmp[0]}
+              'R1Y': d1y_dmp/d1y_dmp[0]}
     
     return irf_z, irf_mp
 
@@ -106,15 +108,15 @@ def get_irf(phi, kappa, ar_z, ma_z, sigma_z, ar_mp, ma_mp, horzs, ss, T):
 """
 
 # Macro moments
-moment_macro = np.hstack((irf_z_data['Z'],irf_z_data['Y'],
-                          irf_mp_data['Y'],irf_mp_data['P'],irf_mp_data['1Y'][1:]))
-moment_macro_se = np.hstack((irf_z_data['Z_se'],irf_z_data['Y_se'],
-                             irf_mp_data['Y_se'],irf_mp_data['P_se'],irf_mp_data['1Y_se'][1:]))
+moment_macro = np.hstack((irf_z_data['TFP'],irf_z_data['GDP'],
+                          irf_mp_data['INDPRO'],irf_mp_data['CPIAUCSL'],irf_mp_data['GS1'][1:]))
+moment_macro_se = np.hstack((irf_z_data['TFP_se'],irf_z_data['GDP_se'],
+                             irf_mp_data['INDPRO_se'],irf_mp_data['CPIAUCSL_se'],irf_mp_data['GS1_se'][1:]))
 moment_macro_num = len(moment_macro)
 
 # Micro moments
-moment_micro = irf_z_data['PctBelowGDP']
-moment_micro_se = irf_z_data['PctBelowGDP_se']
+moment_micro = irf_z_data['BelowCutoff']
+moment_micro_se = irf_z_data['BelowCutoff_se']
 moment_micro_num = len(moment_micro)
 
 # Stack all moments
@@ -130,7 +132,7 @@ def moment_fct(theta, horzs, ss, T):
     
     (phi, kappa, ar_z, ma_z, sigma_z, ar_mp, ma_mp) = tuple(theta)
     irf_z, irf_mp = get_irf(phi, kappa, ar_z, ma_z, sigma_z, ar_mp, ma_mp, horzs, ss, T)
-    return np.hstack((irf_z['Z'],irf_z['Y'],irf_mp['Y'],irf_mp['P'],irf_mp['1Y'][1:],irf_z['PctBelowGDP']))
+    return np.hstack((irf_z['Z'],irf_z['Y'],irf_mp['Y'],irf_mp['P'],irf_mp['R1Y'][1:],irf_z['PctBelowGDP']))
 
 
 def mindist_obj(theta, moment, horzs, ss, T):
@@ -145,7 +147,7 @@ def mindist_obj(theta, moment, horzs, ss, T):
 # Estimation using only macro moments
 weight_mat_macro = np.diag(np.hstack((1/moment_macro_se**2,np.zeros(moment_micro_num)))) # Weight matrix (zeros on micro moments)
 param_bounds = [(1,np.inf),(-np.inf,np.inf),(-1,1),(-np.inf,np.inf),(0,np.inf),(-1,1),(-np.inf,np.inf)] # Parameter bounds
-param_init = np.array([1.5,0.01,0.95,0,irf_z_data['Z'][0]/100,0.5,0]) # Initial parameter guess
+param_init = np.array([1.5,0.01,0.95,0,irf_z_data['TFP'][0]/100,0.5,0]) # Initial parameter guess
 
 print('Running numerical optimization for macro-only estimation...')
 opt_res = opt.minimize(lambda theta: mindist_obj(theta, moment, horzs, ss, T),
@@ -155,6 +157,9 @@ opt_res = opt.minimize(lambda theta: mindist_obj(theta, moment, horzs, ss, T),
                        callback=lambda x: print(np.array_str(x, precision=3, suppress_small=True)))
 param_estim = opt_res['x'] # Parameter estimates
 print('Done.')
+assert opt_res['success']
+print('Convergence message:')
+print(opt_res['message'])
 
 
 #Standard errors
@@ -180,14 +185,18 @@ res_eff = obj.fit(param_estim=param_estim, moment_jacob=res['moment_jacob'], eff
 """
 
 print('Parameter estimates (macro only):')
-print(res['estim'])
+print(np.array_str(res['estim'], precision=3, suppress_small=True))
 print('Worst-case standard errors (macro only):')
-print(res['estim_se'])
+print(np.array_str(res['estim_se'], precision=3, suppress_small=True))
+print('t-stats (macro only):')
+print(np.array_str(res['estim']/res['estim_se'], precision=3, suppress_small=True))
 
 print('Parameter estimates (efficient):')
-print(res_eff['estim'])
+print(np.array_str(res_eff['estim'], precision=3, suppress_small=True))
 print('Worst-case standard errors (efficient):')
-print(res_eff['estim_se'])
+print(np.array_str(res_eff['estim_se'], precision=3, suppress_small=True))
+print('t-stats (efficient):')
+print(np.array_str(res_eff['estim']/res_eff['estim_se'], precision=3, suppress_small=True))
 
 print('Over-ID t-stats:')
 print(res_overid['tstat'])
