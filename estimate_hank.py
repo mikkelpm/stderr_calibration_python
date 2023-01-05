227d0f5f67e8b2fe0c4f44590439c20d3b3e8492
saboutajdine <113407930+saboutaj@users.noreply.github.com>
2023-01-05 21:08:58 +0100

    Running the code using Auclert et al. as a baseline

--- estimate_hank.py
+++ estimate_hank.py
@@ -138,7 +138,7 @@ def moment_fct(theta):
 
 # Estimation using diagonal weight matrix
 param_bounds = [(0,np.inf),(1e-6,np.inf),(-np.inf,np.inf),(-np.inf,np.inf),(0,np.inf),(-np.inf,np.inf),(-np.inf,np.inf)] # Parameter bounds
-param_init = np.array([1.5,0.01,0,0,irf_z_data['TFP'][0]/100,0.5,0]) # Initial parameter guess
+param_init = np.array([1.352,0.144,0.951,0,0.072,0.460,0]) # Initial parameter guess
 
 print('Running numerical optimization for diagonal weight matrix...')
 opt_res = opt.minimize(lambda theta: np.sum(((moment-moment_fct(theta))/moment_se)**2),
@@ -151,40 +151,6 @@ print('Done.')
 assert opt_res['success']
 print('Convergence message:')
 print(opt_res['message'])
-
-# Optimization starting with Auclert et al. estimation
-param_init_Auc = np.array([1.352,0.144,0.951,0,0.072,0.460,0]) # Initial parameter guess
-print('Starting from Auclert et al. estimates...')
-opt_res_Auc = opt.minimize(lambda theta: np.sum(((moment-moment_fct(theta))/moment_se)**2),
-                       param_init_Auc,
-                       method='L-BFGS-B',
-                       bounds=param_bounds,
-                       callback=None)
-param_estim_Auc = opt_res_Auc['x'] # Parameter estimates
-print('Done.')
-assert opt_res_Auc['success']
-print('Convergence message:')
-print(opt_res_Auc['message'])
-
-# Replication of optimization
-param_initbound = [(1,3),(-0.5,0.5),(-1,1),(-1,1),(0.5*irf_z_data['TFP'][0]/100,1.5*irf_z_data['TFP'][0]/100),(-1,1),(-1,1)]
-param_lowerbound = [1,-0.5,-1,-1,0.5*irf_z_data['TFP'][0]/100,-1,-1]
-param_dist = [2,1,2,2,irf_z_data['TFP'][0]/100,2,2]
-param_initgrid = np.random.rand(3,7)
-param_initgrid = np.multiply(param_initgrid,param_dist)+param_lowerbound
-print('Replicating numerical optimization for diagonal weight matrix and using random initial guess...')
-param_estim_repli = np.zeros(np.shape(param_initgrid))
-success=np.zeros(np.shape(param_initgrid)[0])
-for i,guess in enumerate(param_initgrid): 
-    opt_res_repli = opt.minimize(lambda theta: np.sum(((moment-moment_fct(theta))/moment_se)**2),
-                       guess,
-                       method='L-BFGS-B',
-                       bounds=param_bounds,
-                       callback=None)
-    param_estim_repli[i] = opt_res_repli['x'] # Parameter estimates
-    success[i] = opt_res_repli['success']
-print('Done.')
-
 
 #Standard errors
 obj = MinDist(moment_fct, moment, moment_se=moment_se)
